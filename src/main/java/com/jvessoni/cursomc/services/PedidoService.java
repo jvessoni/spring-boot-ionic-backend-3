312079ab8a19eba7688b7450e81ddfc9f2796d06
= <=>
2020-06-18 16:52:43 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/jvessoni/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.jvessoni.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jvessoni.cursomc.domain.Pedido;
+import com.jvessoni.cursomc.repositories.PedidoRepository;
+import com.jvessoni.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+}
