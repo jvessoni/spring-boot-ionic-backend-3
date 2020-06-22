93944fbac47b5a437eb3b5b4a3824c595275e91b
= <=>
2020-06-22 09:50:27 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/jvessoni/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/jvessoni/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
