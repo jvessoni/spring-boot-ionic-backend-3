b3ee57ed98b890757945e6a30f90f879ed879b74
= <=>
2020-06-26 18:05:33 -0300

    Esqueci a senha

--- src/main/java/com/jvessoni/cursomc/resources/AuthResource.java
+++ src/main/java/com/jvessoni/cursomc/resources/AuthResource.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jvessoni.cursomc.dto.EmailDTO;
 import com.jvessoni.cursomc.security.JWTUtil;
 import com.jvessoni.cursomc.security.UserSS;
+import com.jvessoni.cursomc.services.AuthService;
 import com.jvessoni.cursomc.services.UserService;
 
 @RestController
@@ -18,12 +21,21 @@ public class AuthResource {
 
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@RequestBody EmailDTO objDto) {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/jvessoni/cursomc/services/EmailService.java
+++ src/main/java/com/jvessoni/cursomc/services/EmailService.java
@@ -2,6 +2,7 @@ package com.jvessoni.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.jvessoni.cursomc.domain.Cliente;
 import com.jvessoni.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -9,4 +10,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
