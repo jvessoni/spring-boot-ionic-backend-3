00d417edb34c75e76fe4c78dc7d4bc36b1ebe876
= <=>
2020-06-29 17:14:41 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/jvessoni/cursomc/domain/Cliente.java
+++ src/main/java/com/jvessoni/cursomc/domain/Cliente.java
@@ -55,8 +55,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
-
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -152,14 +150,6 @@ public class Cliente implements Serializable {
 		perfis.add(perfil.getCod());
 	}
 	
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
 	@Override
 	public int hashCode() {
 		final int prime = 31;
