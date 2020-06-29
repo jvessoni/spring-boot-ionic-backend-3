168180a87e243de9445452951d0e7520a987b850
= <=>
2020-06-29 15:38:45 -0300

    Tratando exceções adequadamente

---
+++ src/main/java/com/jvessoni/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,15 @@
+package com.jvessoni.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
