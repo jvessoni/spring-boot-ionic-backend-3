429e1784e0bac4460cd4a4d560519bf41ff6e630
= <=>
2020-06-30 10:48:50 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/jvessoni/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jvessoni/cursomc/resources/ClienteResource.java
@@ -44,6 +44,12 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@RequestMapping(value="/email", method=RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@RequestParam(value="value") String email) {
+		Cliente obj = service.findByEmail(email);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@RequestBody Cliente obj, @PathVariable Integer id) {
 //		Cliente obj = service.fromDTO(objDto);
